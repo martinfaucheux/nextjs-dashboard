64ff4705d22d1cb2a6d1bba4d345d1f3080d79c9
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-13 17:57:41 +0100

    add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Sniglet } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const sniglet = Sniglet({ weight: '400' });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
