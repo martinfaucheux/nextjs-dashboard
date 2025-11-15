155fb02145d29381dc27b38cf4b9be0a4491e47c
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-15 17:02:04 +0100

    add dashboard page streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,10 +14,8 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
