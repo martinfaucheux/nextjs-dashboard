353067e7495e15324233308eab05d5ff4ab670c3
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-16 22:06:20 +0100

    add current user in sidebar

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
-import { PowerIcon } from "@heroicons/react/24/outline";
-import { signOut } from "@/auth";
+import { PowerIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { signOut, auth } from "@/auth";
 
-export default function SideNav() {
+export default async function SideNav() {
+  const session = await auth();
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -18,10 +20,13 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <div className="hidden h-[48px] w-full items-center justify-start gap-2  rounded-md bg-gray-50 p-3 text-sm font-medium md:flex md:p-2 md:px-3">
-          <PowerIcon className="w-6" />
-          <div>Hello Anouk</div>
-        </div>
+        {/* display the user only if logged in */}
+        {session && session.user ? (
+          <div className="hidden h-[48px] w-full items-center justify-start gap-2  rounded-md bg-gray-50 p-3 text-sm font-medium md:flex md:p-2 md:px-3">
+            <UserCircleIcon className="w-6" />
+            <div>Hello {session?.user?.name || "User"}</div>
+          </div>
+        ) : null}
         <form
           action={async () => {
             "use server";
