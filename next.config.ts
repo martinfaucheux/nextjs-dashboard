1fff8080630030e7ff1f94c51437d6bd35dae1b1
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 12:12:10 +0100

    add next-pwa lib

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from 'next';
+import { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Your existing Next.js config here
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  // skipWaiting: true, // This option is not available in @ducanh2912/next-pwa
+})(nextConfig);
