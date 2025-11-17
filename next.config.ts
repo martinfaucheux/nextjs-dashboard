95edb51507cc4e355bf0ec0b8d2c01b32683c4b2
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 12:42:58 +0100

    update definition of pwa

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
 const nextConfig: NextConfig = {
   // Your existing Next.js config here
@@ -9,5 +12,4 @@ export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  // skipWaiting: true, // This option is not available in @ducanh2912/next-pwa
 })(nextConfig);
