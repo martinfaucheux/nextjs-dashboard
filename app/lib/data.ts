824b5e35ae4abaf213efeed9889e8b4b146f53a2
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 12:22:33 +0100

    remove artificial wait time

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,10 +13,6 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
@@ -30,10 +26,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    // Artificially delay a response for demo purposes.
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
